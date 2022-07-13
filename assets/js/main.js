f9cc0c572d887271aba3928f6b6a57fa1a2ed4c2
JohnnatanV <joh.villaneda@outlook.com>
2022-07-12 22:13:01 -0500

    Working on the form validation state, still no results

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-console.log("Hola Johnnatan");
+const inputError = document.querySelectorAll(".input-box[data-error] input");
+
+inputError.forEach((dataError) => {
+  dataError.addEventListener("input", () =>
+    dataError.parentElement.removeAttribute("data-error")
+  );
+});
