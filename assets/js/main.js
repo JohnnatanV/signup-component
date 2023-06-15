015b7f42c5460d4a3947fc90c8d4d8d14b7a26f5
JohnnatanV <joh.villaneda@outlook.com>
2023-06-15 01:02:45 -0500

    Actualizacion de proyecto, con cambios y mejora de verificacion de inputs. Pendiente validar password y inicio limpio de inputs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
-const inputError = document.querySelectorAll(".input-box[data-error] input");
+/*const inputError = document.querySelectorAll(".input-box[data-error] input");
 
 inputError.forEach((dataError) => {
   dataError.addEventListener("input", () =>
     dataError.parentElement.removeAttribute("data-error")
   );
+});
+*/
+
+const inputBox = document.querySelectorAll(".input-box input");
+
+console.log(inputBox[0]);
+
+inputBox.forEach((box) => {
+  box.addEventListener("input", () => {
+    if (!box.value) {
+      box.parentElement.classList.add("post-error");
+    } else {
+      box.parentElement.classList.remove("post-error");
+    }
+  });
 });
