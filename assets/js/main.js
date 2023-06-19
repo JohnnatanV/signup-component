445fd975469f7037b0d6265fca6a4725042c71dd
JohnnatanV <joh.villaneda@outlook.com>
2023-06-18 23:21:59 -0500

    DOM manipulation for form validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,40 @@
-/*const inputError = document.querySelectorAll(".input-box[data-error] input");
+const validateForm = document.getElementById("form");
+const inputText = document.querySelectorAll("input[type=text]");
+const inputEmail = document.querySelector("input[type=email]");
+const inputPassword = document.getElementById("password");
 
-inputError.forEach((dataError) => {
-  dataError.addEventListener("input", () =>
-    dataError.parentElement.removeAttribute("data-error")
-  );
-});
-*/
-
-const inputBox = document.querySelectorAll(".input-box input");
-
-console.log(inputBox[0]);
-
-inputBox.forEach((box) => {
-  box.addEventListener("input", () => {
-    if (!box.value) {
+validateForm.addEventListener("change", () => {});
+inputText.forEach((box) => {
+  box.addEventListener("change", (event) => {
+    if (!event.target.value) {
       box.parentElement.classList.add("post-error");
+      box.nextElementSibling.classList.remove("hidden");
     } else {
       box.parentElement.classList.remove("post-error");
+      box.nextElementSibling.classList.add("hidden");
     }
   });
 });
+
+let rxEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+.[^@ \t\r\n]+/;
+let rxPass = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$_%^&*-]).{8,}$/g;
+
+inputEmail.addEventListener("input", (event) => {
+  if (!rxEmail.test(event.target.value)) {
+    inputEmail.parentElement.classList.add("post-error");
+    inputEmail.nextElementSibling.classList.remove("hidden");
+  } else {
+    inputEmail.parentElement.classList.remove("post-error");
+    inputEmail.nextElementSibling.classList.add("hidden");
+  }
+});
+
+// inputPassword.addEventListener("change", (event) => {
+//   if (rxPass.test(event.target.value)) {
+//     inputPassword.parentElement.classList.remove("post-error");
+//     inputPassword.nextElementSibling.classList.add("hidden");
+//   } else {
+//     inputPassword.parentElement.classList.add("post-error");
+//     inputPassword.nextElementSibling.classList.remove("hidden");
+//   }
+// });
